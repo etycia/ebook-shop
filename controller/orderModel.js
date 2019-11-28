e0d094424b1d95846f78199c0621327ab98c074a
Laetitia O <Laetitiao@MacBook-Pro-de-Laetitia.local>
2019-11-28 09:54:43 +0100

    Connection Database ebook + order model + ejs views

---
+++ controller/orderModel.js
@@ -0,0 +1,54 @@
+// Module 1
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var orderSchema = new Schema({
+    name: String
+});
+
+
+
+
+// Module 2
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var order = new Schema({
+    orderDate: Date,
+    EUR_amount : Number,
+    ebook:[
+      {type: Schema.Types.ObjectId, ref: 'ebooks'}
+    ],
+    user:[
+      {type: Schema.Types.ObjectId, ref: 'users'}
+});
+
+
+
+async function addOrder (productId, userId, EUR_amount){
+
+    var o = new order();
+
+    o.orderDate = '2019-11-28';
+    o.ebook.push(productId);
+    o.user.push(userId);
+
+    o.save();
+}
+
+async function getUserOrders(userId){
+    var userOrder = new order.find({user : userId});
+    
+}
+async function getUserProducts(orderId){
+    var userProductsIdFromOrder = new order.find({_id : orderId });
+    // Todo find array of id?
+    var productOfOrder = new ebook.find();
+    // foreach + Tableau ?
+}
+
+
+module.exports = mongoose.model('order', orderSchema);
+mongoose.model('order', orderSchema);
--- index.js
+++ index.js
@@ -1,50 +1,110 @@
-// init  modules call
+//init 
+var express = require('express');
+const ejs = require('ejs')
+const app = express();
+var path = require("path");
+var bodyParser = require("body-parser");
+var hostname = '127.0.0.1';
+var port = 3000;
 let fs = require('fs');
 let readline = require('readline');
-
 const order = require(__dirname + '/controller/order.js');
-const file = "data/products.json";
-let book;
-//const tpl = "view/products.tpl";
+//const products = require(__dirname + '/data/products.js');
+var productIdToBuy = '';
+// init end
 
-//console.log(order);
 
-// Reading products file
-fs.readFile(file, 'utf8', (_err, file) => {
-    book = JSON.parse(file);
-    console.log("Bienvenue\nVoici les produits disponibles :\n");
-    start(book);
+// Middleware
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+})); 
+app.use('/public', express.static(__dirname + '/public'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+
+
+// MongoDB
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/ebook', {useNewUrlParser: true,  useUnifiedTopology: true});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("we're connected!");
 });
 
-// encapsultate into a function for delayed function call
-function start(book){
-    getAllProducts(book);
+// schema
+var ebookSchema = new mongoose.Schema({
+  name: String,
+  id: String,
+  description: String,
+  USD_price: Number,
+  EUR_price: Number,
+  file_link: String,
+  creation_date: Date,
+  orders_counter: Number
+  
+});
 
-    // Stdin listen
-    var rl = readline.createInterface({input: process.stdin, output: process.stdout, terminal: false});
-    rl.on("line",(id)=>{
-        order.orderProductById(id, book, file);
-    });
-    
-   
-};
+// ###MongoDB
 
-// Function get all products
-function getAllProducts(book){
-    //var result = "";
-    book.forEach(element => {
-        console.log(`# ${element.id} - ${element.name} / ${element.EUR_price} / ${element.orders_counter} \n`);
-    });
+var ebook = mongoose.model('ebook', ebookSchema);
+var bookToSell = new ebook();
+
+async function getBookById(id){
+    return await ebook.find({_id : id});
 }
 
 
+async function getAllBook(){
+    return await ebook.find({});
+}
+
+//      save book
+function saveBook(objBook){
+    var book = new ebook(objBook);
+    
+    book.save(function (err, book) {
+        if (err) return console.error(err);
+      });
+}
+
+//
+//app.post('/', (req, res) => res.send('What do you want?'));
+
+app.get('/', async (req, res) => {
+    var books = await getAllBook();
+    res.render('index', {books:books});
+});
+
+app.get('/login', async (req, res) => {
+    //var books = await getAllBook();
+    res.render('login', {});
+});
 
 
+app.post('/', (req, res) => {
+    var bookId = req.body.id;
+    console.log(req.body);
+    res.sendStatus(200);
+    getBookById(bookId);
+});
 
+app.post('/login', (req, res) => {
+    var username = req.body.username;
+    var pwd = req.body.pwd;
+    console.log(req.body);
+    res.sendStatus(200);
+    // controler l'authentification
 
+});
 
+app.listen(port, () => console.log(`Ebook shop app listening on port ${port}`));
 
-
-
-
-
+app.all('/secret', (req, res, next) => {
+    res.send('Accessing the secret section ...');
+    console.log('Accessing the secret section ...');
+    next(); // pass control to the next handler
+  });
